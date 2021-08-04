b46249b065fb5f67bb35e5cb235cf37f7c355647
George <dgeorge@celestica.com>
2021-08-04 13:33:54 +0800

    add two sets of brixia deployment scripts

--- ansible/module_utils/port_utils.py
+++ ansible/module_utils/port_utils.py
@@ -158,6 +158,9 @@ def get_port_alias_to_name_map(hwsku, asic_id=None):
     elif hwsku == "Celestica-E1031-T48S4":
         for i in range(1, 53):
             port_alias_to_name_map["etp%d" % i] = "Ethernet%d" % ((i - 1))
+    elif hwsku == "Brixia":
+        for i in range(1, 33):
+            port_alias_to_name_map["OSFP%d" % i] = "Ethernet%d" % ((i - 1) * 4)
     elif hwsku == "et6448m" or hwsku == "Nokia-7215":
         for i in range(0, 52):
             port_alias_to_name_map["Ethernet%d" % i] = "Ethernet%d" % i
