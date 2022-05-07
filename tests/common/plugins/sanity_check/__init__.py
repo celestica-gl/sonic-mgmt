64437f4efaa23fb164a850a112a6e50fcbdc0641
119064273 <2276096708@qq.com>
2022-05-07 03:49:20 +0000

    fix brixia t0 t1 config

--- tests/common/plugins/sanity_check/__init__.py
+++ tests/common/plugins/sanity_check/__init__.py
@@ -101,7 +101,7 @@ def do_checks(request, check_items, *args, **kwargs):
     return check_results
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="module", autouse=False)
 def sanity_check(localhost, duthosts, request, fanouthosts, nbrhosts, tbinfo):
     logger.info("Prepare sanity check")
 
